a8f6631a99959b95f5bc253589b00acd56b55dbf
abdullahsunca <abdullah.sunca@beyn.com.tr>
2020-05-31 23:06:19 +0300

    Initial commit

---
+++ precache-manifest.5f5a03fdb027e229ad8d6b9ceef21f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f867dc4cac2759c291e8fae2c13c5784",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25ec0fb2733bf82352bf",
+    "url": "/static/css/main.309d016a.chunk.css"
+  },
+  {
+    "revision": "9270d6b12bd8a97d833f",
+    "url": "/static/js/2.790a5a8b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.790a5a8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ec0fb2733bf82352bf",
+    "url": "/static/js/main.def8bd54.chunk.js"
+  },
+  {
+    "revision": "5fec4345b2006998b2b8",
+    "url": "/static/js/runtime-main.3226aa9b.js"
+  }
+]);
